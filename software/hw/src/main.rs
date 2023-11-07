f5edb0c737264c6eefe03e78512f5deee7cc59e8
BrotherDan Boyce <danboyce@sonic.net>
2023-11-07 15:27:56 -0800

    Font change

--- software/hw/src/main.rs
+++ software/hw/src/main.rs
@@ -1,14 +1,35 @@
 use esp_idf_hal::i2c::*;
 use esp_idf_hal::prelude::*;
-use esp_idf_hal::{delay::FreeRtos, gpio::PinDriver, peripherals::Peripherals};
+use esp_idf_hal::{delay::FreeRtos,  peripherals::Peripherals};
 use esp_idf_sys as _;
 use ht16k33::{LedLocation, HT16K33};
+
+pub mod font;
+use font::char_to_u64;
+/// Map logical matrix location to actual device coordinate 
+///
+/// Writing to location (0,0) in the display doesn't actually cause the corner
+/// LED to light up. Instead, the LED at location (0,6) lights up. To make it
+/// easier to work with the matrix, the DISPLAY_MAP returns the *actual* 
+/// display x/y coordinates needed to a particular *logical* matrix location
+#[rustfmt::skip]
+const DISPLAY_MAP: [[(u8, u8); 8]; 8] = [
+    [(0,6),(2,6),(4,6),(6,6),(8,6),(10,6),(12,6),(14,6)],
+    [(0,5),(2,5),(4,5),(6,5),(8,5),(10,5),(12,5),(14,5)],
+    [(0,4),(2,4),(4,4),(6,4),(8,4),(10,4),(12,4),(14,4)],
+    [(0,3),(2,3),(4,3),(6,3),(8,3),(10,3),(12,3),(14,3)],
+    [(0,2),(2,2),(4,2),(6,2),(8,2),(10,2),(12,2),(14,2)],
+    [(0,1),(2,1),(4,1),(6,1),(8,1),(10,1),(12,1),(14,1)],
+    [(0,0),(2,0),(4,0),(6,0),(8,0),(10,0),(12,0),(14,0)],
+    [(0,7),(2,7),(4,7),(6,7),(8,7),(10,7),(12,7),(14,7)],
+];
+
 
 fn main() {
     esp_idf_sys::link_patches();
 
     let peripherals = Peripherals::take().unwrap();
-    let mut led_pin = PinDriver::output(peripherals.pins.gpio7).unwrap();
+    // let mut led_pin = PinDriver::output(peripherals.pins.gpio7).unwrap();
 
     let sda = peripherals.pins.gpio10;
     let scl = peripherals.pins.gpio8;
@@ -23,38 +44,67 @@ fn main() {
     ht16k33.initialize().unwrap();
     ht16k33.set_display(ht16k33::Display::ON).unwrap();
 
-    #[rustfmt::skip]
-    let display_map = [
-        [(0,6),(2,6),(4,6),(6,6),(8,6),(10,6),(12,6),(14,6)],
-        [(0,5),(2,5),(4,5),(6,5),(8,5),(10,5),(12,5),(14,5)],
-        [(0,4),(2,4),(4,4),(6,4),(8,4),(10,4),(12,4),(14,4)],
-        [(0,3),(2,3),(4,3),(6,3),(8,3),(10,3),(12,3),(14,3)],
-        [(0,2),(2,2),(4,2),(6,2),(8,2),(10,2),(12,2),(14,2)],
-        [(0,1),(2,1),(4,1),(6,1),(8,1),(10,1),(12,1),(14,1)],
-        [(0,0),(2,0),(4,0),(6,0),(8,0),(10,0),(12,0),(14,0)],
-        [(0,7),(2,7),(4,7),(6,7),(8,7),(10,7),(12,7),(14,7)],
+    let frames = [
+        // // The following two function calls produce the same results
+        // create_matrix(
+        //     "
+        // 11111111
+        // 00000000
+        // 00000000
+        // 00000000
+        // 00000000
+        // 00000000
+        // 00000000
+        // 00000000
+        // ",
+        // ),
+        // create_matrix_from_pattern(["1", "0", "0", "0", "0", "0", "0", "0"]),
+        char_to_u64('H'),
+        char_to_u64('i'),
+        char_to_u64(' '),
+        font::P,
+        font::o,
+        font::p,
+        //char_to_u64('p'),
+        char_to_u64('!'),
+        char_to_u64('*'),
     ];
-    // let led_location = LedLocation::new(g.0, g.1).unwrap();
-    loop {
-        // ht16k33.update_display_buffer(led_location, true);
-        // ht16k33.write_display_buffer().unwrap();
-        // ht16k33.update_display_buffer(led_location, false);
-        // ht16k33.write_display_buffer().unwrap();
-        for i in 0..8 {
-            for j in 0..8 {
-                let g = display_map[i][j];
-                let led_location = LedLocation::new(g.0, g.1).unwrap();
-                ht16k33.clear_display_buffer();
-                ht16k33.update_display_buffer(led_location, true);
-                ht16k33.write_display_buffer().unwrap();
-                FreeRtos::delay_us(1000);
-            }
-        }
-        // FreeRtos::delay_ms(1000);
-        // led_pin.set_low().unwrap();
 
-        // led_pin.set_high().unwrap();
-        // FreeRtos::delay_ms(1000);
-        // println!("blink");
+    loop {
+        // Each frame is a 64-bit number, where each bit represents the on/off
+        // state of an LED.
+        for frame in frames {
+            ht16k33.clear_display_buffer();
+            // Iterate over each bit in the current frame
+            for i in 0..64 {
+                // i / 8 will increase by one every 8 iterations:
+                // i    : 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+                // i / 8: 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1
+                //
+                // i % 8 just repeatedly counts 0-7:
+                // i    : 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+                // i % 8: 0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2
+                //
+                // Using these two numbers, we can easily walk over each index
+                // in the matrix.
+                let (x, y) = DISPLAY_MAP[i / 8][i % 8];
+
+                //
+                let led_location = LedLocation::new(x, y).unwrap();
+
+                // Use bit shifting logic to determine if the current bit is set
+                // or not. We use the bitwise shift `>>` to move the `i`th bit
+                // into the "ones" place. We then bitwise "and" operator `&`
+                // to retain only the first bit in our result. Lastly, we
+                // check if our sole bit is equal to 1 or not.
+                let should_be_on = frame >> i & 1 == 1;
+
+                ht16k33.update_display_buffer(led_location, should_be_on);
+            }
+            ht16k33.write_display_buffer().unwrap();
+            FreeRtos::delay_ms(400);
+        }
+        FreeRtos::delay_ms(1000);
     }
 }
+
